a85676b0b307d1dca942259d3ee7d7dbb9d1f734
daniemart <github email address>
2019-08-28 15:03:37 -0700

    added assocatiations

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
     has_many: plans
     has_many: favorites 
+    has_many :restaurants, through: :favorites, source: :favorite, source_type: 'restaurant'
+    has_many :events, through: :favorites, source: :favorite, source_type: 'event'
+
 
 end

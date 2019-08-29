5807c42ff0403cd54916a31d360894ea041a2999
daniemart <github email address>
2019-08-29 10:13:17 -0700

    im stupid

--- backend/app/controllers/restaurants_controller.rb
+++ backend/app/controllers/restaurants_controller.rb
@@ -1,2 +1,11 @@
 class RestaurantsController < ApplicationController
+    def index
+        @restaurants = Restaurant.all
+        render @restaurant, except: [:created_at, :updated_at]
+    end 
+
+    def show
+        @restaurant = Restaurant.find(params[:id])
+        render @restaurant, except: [:created_at, :updated_at]
+    end
 end
--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -1,2 +1,31 @@
 class UsersController < ApplicationController
+
+    def index
+        @users = User.all
+        render @user, except: [:created_at, :updated_at]
+    end 
+
+    def show
+        @user = User.find(params[:id])
+        render @user, except: [:created_at, :updated_at]
+    end 
+
+    def new
+        @user = User.new
+    end 
+
+    def created
+        @user = User.new(name:params[:name])
+        @user.save
+        render @user, except: [:created_at, :updated_at]
+      
+    end 
+
+    def destory
+        @user = User.find(params[id])
+        @user.delete
+        session.clear
+        render @user, except: [:created_at, :updated_at]
+    end 
+
 end

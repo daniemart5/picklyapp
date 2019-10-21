885124441c1cd33c8fd69b779aaff174c504f003
daniemart <github email address>
2019-10-20 17:18:39 -0700

    added linkes to social media

--- frontend/frontend_stuff/src/pages/About.js
+++ frontend/frontend_stuff/src/pages/About.js
@@ -5,13 +5,16 @@ import profile  from '../layout/Profile.jpg';
 class About extends Component {
     render() {
         return (
-            <div>
+            <div className="about">
                 <Header/>
                 <h1>About</h1>
                 <span><img className='DANIE' src={profile}/></span>
                 <h2>Danielle Martinoli</h2>
 
                 <p>A software developer, living in the heart of the emerald city!</p>
+
+                <p><a href="https://github.com/daniemart5/">Github</a></p>
+                <p><a href="https://www.linkedin.com/in/daniemartinoli/">LinkedIn</a></p>
             </div>
         )
     }
